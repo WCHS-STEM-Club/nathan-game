61113015543fd9b2a4cdc3b9adbdf98fb66f3e5e
medude <medude@github.com>
2018-02-16 07:16:48 -0500

    Added something

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 let friction = 0.9;
-let gravity = 0.3;
-let movePower = 1;
-
-Physijs.scripts.worker = '/js/physijs_worker.js';
-Physijs.scripts.ammo = '/js/ammo.js';
+let gravity = -0.1;
+let movePower = 0.1;
 
 const renderer = new THREE.WebGLRenderer({
     antialias: true
 });
-const scene = new Physijs.Scene();
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     35,
     window.innerWidth / window.innerHeight,
@@ -17,43 +14,40 @@ const camera = new THREE.PerspectiveCamera(
 );
 const light = new THREE.PointLight(0xffffff);
 
-const floor = new THREE.Mesh(
-    new THREE.BoxGeometry(10, 5, 1),
-    new THREE.MeshLambertMaterial({
-        color: 0x00ff00
-    })
-);
+/***********
+ * Physics *
+ ***********/
+const engine = new Matter.Engine.create();
+engine.world.gravity.y = gravity;
+const ground = new Ground(scene);
+const player = new Player(scene);
+
+Matter.World.add(engine.world, [ground.physics, player.physics]);
 
 function initScene() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-
     light.position.set(10, 1, 10);
+
     scene.add(light);
 
-    floor.position.y = -3;
-    scene.add(floor);
-
     camera.position.set(0, 0, 20);
+
     scene.add(camera);
-
     animate();
-}
 
-let player = new Player(scene);
-player.v.x = 1;
+}
 
 initScene();
 
 function animate() {
     requestAnimationFrame(animate);
-    player.calcNewPos();
-    player.syncMesh();
+    Matter.Engine.run(engine);
+    player.sync();
     camera.position.x = player.pos.x;
     light.position.x = player.pos.x + 10;
     renderer.render(scene, camera);
 }
-// animate();
 
 window.addEventListener("keydown", (e) => {
     const UP = 38;
@@ -62,10 +56,10 @@ window.addEventListener("keydown", (e) => {
     if (player.state !== PlayerStates.IN_AIR || gravity === 0) {
         switch (e.keyCode) {
             case UP:
-                player.v.y += movePower;
+                Matter.Body.applyForce(player.physics, {x: 0, y: 1}, {x: 0, y: movePower});
                 break;
             case DOWN:
-                player.v.y -= movePower;
+                Matter.Body.applyForce(player.physics, {x: 0, y: -1}, movePower);
                 break;
         }
     }
